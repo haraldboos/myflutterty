e62dc1de72081a2107b8744d031fa022e4ff02e0
haraldboos <haraldboos16@gmail.com>
2024-10-14 22:35:30 +0530

    learning dart

---
+++ 1.dart
@@ -0,0 +1,23 @@
+String fuck = "this Funckljd";
+bool istruert = false;
+
+List nameya =["sujitha","oohyah"];
+ Map ohyah = {
+  "ooom":"ool"
+ };
+var miya = "oooootha";
+void main(){
+print(miya.isEmpty);
+final all =  lover("sugitha");
+final add = addtion(12, 34);
+print(add);
+print(all);
+}
+
+String lover(String lov){
+  print("had ❤️ ${lov}");
+  return "hld ❤️ ${lov}";
+}
+int addtion(int a,int b){
+  return a+b;
+}
