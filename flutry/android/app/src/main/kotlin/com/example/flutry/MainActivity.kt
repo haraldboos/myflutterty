6d3c7a31aaaa101bc5e14fad27f614e5abc9f957
haraldboos <haraldboos16@gmail.com>
2025-03-26 23:42:35 +0530

    reshaping the flutter

---
+++ flutry/android/app/src/main/kotlin/com/example/flutry/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutry/lib/main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home:  Scaffold(
+    appBar: AppBar(title: Text("hey babe",style: TextStyle(color: Colors.amber[300],
+    )
+    ),
+    ),
+    body: ListView(
+      children: [
+        Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    )
+      ,
+      Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    )
+    ,Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    )
+    ,Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    ),
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    )
+    ,
+    Container(
+      height: 90,
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      decoration: BoxDecoration(color: Colors.white,boxShadow: [BoxShadow(spreadRadius: 1,blurRadius: 5,color: Colors.black12,offset: Offset(0, 5))],borderRadius: BorderRadius.circular(4)),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center,crossAxisAlignment: CrossAxisAlignment.start,children: [Text("hi da",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),),Text("pnda"),Text("andi",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold ),)],),
+    )],
+    ))
+    ,)
+    );
+}
